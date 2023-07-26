5d516a5fc2e08d358aa781f943a42c6c33ee1ce9
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2023-07-26 13:25:13 +0300

    Commit

---
+++ src/HW/1/UserList.tsx
@@ -0,0 +1,30 @@
+type AddressType = {
+  street: string;
+  city: string;
+};
+
+type UserType = {
+  id: number,
+  name: string,
+  age: number,
+  address: AddressType
+};
+type UserListPropsType = {
+  users: UserType[]; // ПО МОЕМУ ЧЕГО-ТО НЕ ХВАТАЕТ...
+};
+
+export const UserList = (props: UserListPropsType) => {
+  return (
+    <div id={'hw01-users'}>
+      <h2>User List:</h2>
+      <ul>
+        {props.users.map((user) => ( // ВСЕ ТОВАРЫ В СТРАНУ ПРИЕЗЖАЮТ В КОНТЕЙНЕРАХ, А В РЕАКТЕ...
+          <li key={user.id} id={`hw01-user-${user.id}`}>
+            <strong>{user.name}</strong> (Age: {user.age})<strong> Address:</strong>
+            {user.address.street}, {user.address.city}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
